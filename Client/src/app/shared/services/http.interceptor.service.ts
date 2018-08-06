fcdb2bc647bc02a89a6e983c2df2555b653a265c
amondal <apurbamondal@icloud.com>
2018-08-06 22:57:41 +0530

    .

--- Client/src/app/shared/services/http.interceptor.service.ts
+++ Client/src/app/shared/services/http.interceptor.service.ts
@@ -50,9 +50,6 @@ export class InterceptService implements HttpInterceptor {
         if(error.status == 401){
           this.route.navigate(['/login']);
           console.error('not logged in');
-        }else{
-          this.route.navigate(['/login']);
-          console.log('you internet is Down ');
         }
        
       })
