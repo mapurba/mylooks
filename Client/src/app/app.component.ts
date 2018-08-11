746ceb3cc4be229dc7a80dd4275d0e788c8c1f77
Apurba Mondal <apurbamondal@Apurbas-MacBook-Air.local>
2018-08-12 00:19:01 +0530

    .

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component ,OnInit} from '@angular/core';
 import { UserDetailService } from './shared/services/user-detail.service';
+ import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
