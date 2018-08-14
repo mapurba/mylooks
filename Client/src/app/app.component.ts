db20b9492822be7c2f9265a96184615e5a6943ab
System Administrator <root@Apurbas-Air.Dlink>
2018-08-14 19:36:21 +0530

    .

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component ,OnInit} from '@angular/core';
 import { UserDetailService } from './shared/services/user-detail.service';
  import { Router } from '@angular/router';
+import { CookiesService } from 'src/app/shared/services/utilities/util_cookies/cookies.service';
 
 
 @Component({
@@ -11,7 +12,10 @@ import { UserDetailService } from './shared/services/user-detail.service';
 export class AppComponent implements OnInit{
   title = 'app';
 
-  constructor(private UserDetailService:UserDetailService,private router:Router) { }
+  constructor(private UserDetailService:UserDetailService,private router:Router,private cookiesService:CookiesService) {
+    console.log(this.cookiesService.getCookie('connect'));
+
+   }
   
   
 
