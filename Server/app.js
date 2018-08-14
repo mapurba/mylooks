1715e4e4f48cf28bc20bb7500044459f13671451
Apurba Mondal <apurbamondal@Apurbas-Air.Dlink>
2018-08-14 22:40:11 +0530

    .

--- Server/app.js
+++ Server/app.js
@@ -164,8 +164,8 @@ app.get('/api', apiController.getApi);
 app.get('/api/importInstagramPhotos', passportConfig.isAuthenticated,  apiController.getFacebook);
 
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-app.get('/auth/facebook/callback',passport.authenticate('facebook', {},(req, res) => {
-res.cookie('user',req); 
+app.get('/auth/facebook/callback',passport.authenticate('facebook', (err,req, res) => {
+// res.cookie('user',req); 
 console.log(req); 
 res.redirect('https://buymylooks.westeurope.cloudapp.azure.com/');
 }));
