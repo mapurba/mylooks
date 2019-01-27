b5c9d27679d8d889779d3c69994b9a40e5ddb47c
mapurba <apurbamondal@icloud.com>
2019-01-28 02:46:18 +0530

    login chaneges and backedn changes.

--- Server/controllers/admin.js
+++ Server/controllers/admin.js
@@ -13,17 +13,16 @@ exports.approveTask =(req,res)=>{
 
     let BlogPhotosId=[];
     let newBlogMedias=req.body.usermedia;
-
-    // for (const iterator of newBlogMedias) {
-    //     BlogPhotosId.push({user:req.body.user.id,BlogPhotoId:iterator.id,productLink:iterator.productLink});
-    // }
-
+    for (const key in newBlogMedias) {
+        if(newBlogMedias[key].productLink.length<=0)
+        newBlogMedias.splice(key,1);
+    }
     async.eachSeries(newBlogMedias, function updateObject (obj, done) {
         // Model.update(condition, doc, callback)
         console.log('updating userphoto',obj.id);
-        if(obj.productLink.length>0){
+        // if(obj.productLink.length>0){
             UserMedias.update({ id: obj.id }, { $set : { productLink: obj.productLink ,isApproved:true}}, done);
-        }
+        // }
     }, function allDone (err) {
         // this will be called when all the updates are done or an error occurred during the iteration
         if(err){
