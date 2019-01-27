076372d6b0e8d1ac1c5420c03bcb69fbb3dfeac4
mapurba <apurbamondal@icloud.com>
2019-01-28 02:53:00 +0530

    admin changes

--- Server/controllers/admin.js
+++ Server/controllers/admin.js
@@ -9,35 +9,34 @@ exports.getAllTask = (req, res) => {
 
 };
 
-exports.approveTask =(req,res)=>{
+exports.approveTask = (req, res) => {
 
-    let BlogPhotosId=[];
-    let newBlogMedias=req.body.usermedia;
+    let BlogPhotosId = [];
+    let newBlogMedias = req.body.usermedia;
     for (const key in newBlogMedias) {
-        if(newBlogMedias[key].productLink.length<=0)
-        newBlogMedias.splice(key,1);
+        if (newBlogMedias[key].productLink.length <= 0) { newBlogMedias.splice(key, 1); }
     }
-    async.eachSeries(newBlogMedias, function updateObject (obj, done) {
+    async.eachSeries(newBlogMedias, function updateObject(obj, done) {
         // Model.update(condition, doc, callback)
-        console.log('updating userphoto',obj.id);
+        console.log('updating userphoto', obj.id);
         // if(obj.productLink.length>0){
-            UserMedias.update({ id: obj.id }, { $set : { productLink: obj.productLink ,isApproved:true}}, done);
+        UserMedias.update({ id: obj.id }, { $set: { productLink: obj.productLink, isApproved: true } }, done);
         // }
-    }, function allDone (err) {
+    }, function allDone(err) {
         // this will be called when all the updates are done or an error occurred during the iteration
-        if(err){
+        if (err) {
             res.sendStatus(489);
         }
-        res.status(200).send({msg:'done'});
+        res.status(200).send({ msg: 'done' });
     });
-    
 
-   // res.send(200);
+
+    // res.send(200);
 };
 
-exports.getallblog=(req,res)=>{
+exports.getallblog = (req, res) => {
 
-    BlogPhotos.find({},(err,result)=>{
+    BlogPhotos.find({}, (err, result) => {
         res.status(200).send(result);
     })
 };
